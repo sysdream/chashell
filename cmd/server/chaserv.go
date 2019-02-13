a5fe6eb1dcef67fffa88225a585d282490a4a8e3
Nicolas Chatelain <n.chatelain@sysdream.com>
2019-02-13 10:34:31 +0100

    Migrate cli

--- cmd/server/chaserv.go
+++ cmd/server/chaserv.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"chashell/lib/crypto"
 	"chashell/lib/protocol"
-	"chashell/lib/transport"
 	"encoding/hex"
 	"fmt"
 	"github.com/c-bata/go-prompt"
 	"github.com/golang/protobuf/proto"
 	"github.com/miekg/dns"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -178,86 +175,6 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	w.WriteMsg(m)
 }
 
-func interact(sessionID string) {
-	fmt.Println(consoleBuffer[sessionID])
-	delete(consoleBuffer, sessionID)
-
-	currentSession = sessionID
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		if scanner.Text() == "background" {
-			return
-		}
-		initPacket, dataPackets := transport.Encode([]byte(scanner.Text()+"\n"), false, encryptionKey, targetDomain, nil)
-		_, valid := packetQueue[sessionID]
-		if !valid {
-			packetQueue[sessionID] = make([]string, 0)
-		}
-		packetQueue[sessionID] = append(packetQueue[sessionID], initPacket)
-		for _, packet := range dataPackets {
-			packetQueue[sessionID] = append(packetQueue[sessionID], packet)
-		}
-
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
-}
-
-var commands = []prompt.Suggest{
-	{Text: "sessions", Description: "Interact with the specified machine."},
-	{Text: "exit", Description: "Stop the Chashell Server"},
-}
-
-func Completer(d prompt.Document) []prompt.Suggest {
-	if d.TextBeforeCursor() == "" {
-		return []prompt.Suggest{}
-	}
-	args := strings.Split(d.TextBeforeCursor(), " ")
-
-	return argumentsCompleter(args)
-}
-
-func argumentsCompleter(args []string) []prompt.Suggest {
-	if len(args) <= 1 {
-		return prompt.FilterHasPrefix(commands, args[0], true)
-	}
-
-	first := args[0]
-	switch first {
-	case "sessions":
-		second := args[1]
-		if len(args) == 2 {
-			sessions := []prompt.Suggest{}
-			for clientGUID, clientInfo := range sessionsMap {
-				sessions = append(sessions, prompt.Suggest{Text: clientGUID, Description: clientInfo.hostname})
-			}
-
-			return prompt.FilterHasPrefix(sessions, second, true)
-		}
-
-	}
-	return []prompt.Suggest{}
-}
-
-func executor(in string) {
-	args := strings.Split(in, " ")
-	if len(args) > 0 {
-		switch args[0] {
-		case "exit":
-			fmt.Println("Exiting.")
-			os.Exit(0)
-		case "sessions":
-			if len(args) == 2 {
-				fmt.Printf("Interacting with session %s.\n", args[1])
-				interact(args[1])
-			} else {
-				fmt.Println("sessions [id]")
-			}
-		}
-	}
-}
-
 func main() {
 
 	go func() {
@@ -294,7 +211,4 @@ func main() {
 
 	p := prompt.New(executor, Completer, prompt.OptionPrefix("chashell >>> "))
 	p.Run()
-
-	//ShellServer()
-
 }
---
+++ cmd/server/cli.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"chashell/lib/transport"
+	"fmt"
+	"github.com/c-bata/go-prompt"
+	"os"
+	"strings"
+)
+
+func interact(sessionID string) {
+	fmt.Println(consoleBuffer[sessionID])
+	delete(consoleBuffer, sessionID)
+
+	currentSession = sessionID
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "background" {
+			return
+		}
+		initPacket, dataPackets := transport.Encode([]byte(scanner.Text()+"\n"), false, encryptionKey, targetDomain, nil)
+		_, valid := packetQueue[sessionID]
+		if !valid {
+			packetQueue[sessionID] = make([]string, 0)
+		}
+		packetQueue[sessionID] = append(packetQueue[sessionID], initPacket)
+		for _, packet := range dataPackets {
+			packetQueue[sessionID] = append(packetQueue[sessionID], packet)
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+var commands = []prompt.Suggest{
+	{Text: "sessions", Description: "Interact with the specified machine."},
+	{Text: "exit", Description: "Stop the Chashell Server"},
+}
+
+func Completer(d prompt.Document) []prompt.Suggest {
+	if d.TextBeforeCursor() == "" {
+		return []prompt.Suggest{}
+	}
+	args := strings.Split(d.TextBeforeCursor(), " ")
+
+	return argumentsCompleter(args)
+}
+
+func argumentsCompleter(args []string) []prompt.Suggest {
+	if len(args) <= 1 {
+		return prompt.FilterHasPrefix(commands, args[0], true)
+	}
+
+	first := args[0]
+	switch first {
+	case "sessions":
+		second := args[1]
+		if len(args) == 2 {
+			sessions := []prompt.Suggest{}
+			for clientGUID, clientInfo := range sessionsMap {
+				sessions = append(sessions, prompt.Suggest{Text: clientGUID, Description: clientInfo.hostname})
+			}
+
+			return prompt.FilterHasPrefix(sessions, second, true)
+		}
+
+	}
+	return []prompt.Suggest{}
+}
+
+func executor(in string) {
+	args := strings.Split(in, " ")
+	if len(args) > 0 {
+		switch args[0] {
+		case "exit":
+			fmt.Println("Exiting.")
+			os.Exit(0)
+		case "sessions":
+			if len(args) == 2 {
+				fmt.Printf("Interacting with session %s.\n", args[1])
+				interact(args[1])
+			} else {
+				fmt.Println("sessions [id]")
+			}
+		}
+	}
+}
